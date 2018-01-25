79f923d889de94f1cf9aa741884dba101d8fb3bd
U-BYTE\BYTE-PC <sid.achavezperez@gmail.com>
2018-01-25 14:21:47 -0600

    Yes

--- src/pages/detalle/detalle.component.ts
+++ src/pages/detalle/detalle.component.ts
@@ -1,11 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { Lista, ListaItem } from '../../app/clases';
+import { ListaDeseosService } from '../../app/services/lista-deseos.service';
 
 @Component({
   selector: 'app-detalle',
   templateUrl: 'detalle.component.html',
 })
 export class DetalleComponent implements OnInit {
-  constructor() {  }
+
+  idx:number;
+  lista:Lista;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public _listaDeseos: ListaDeseosService
+  ) {
+
+    this.idx = this.navParams.get('idx');
+    this.lista = this.navParams.get('lista');
+
+  }
 
   ngOnInit() {}
+
+  actualizar( item:ListaItem ){
+
+    item.completado = !item.completado;
+
+    this._listaDeseos.actualizarData();
+
+  }
+
 }
