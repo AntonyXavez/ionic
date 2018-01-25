73ac0516aa9cf31dcc9b7e554af78aa8d6878bc1
U-BYTE\BYTE-PC <sid.achavezperez@gmail.com>
2018-01-25 11:39:22 -0600

    what is

--- src/pages/pendiente/pendientes.component.ts
+++ src/pages/pendiente/pendientes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ListaDeseosService } from '../../app/services/lista-deseos.service';
 import { NavController } from 'ionic-angular';
 import { AgregarComponent } from '../agregar/agregar.component';
+import { DetalleComponent } from '../detalle/detalle.component';
 
 @Component({
   selector: 'app-pendientes',
@@ -20,5 +21,9 @@ export class PendientesComponent implements OnInit {
 
     this.navCtrl.push( AgregarComponent );
 
+  }
+
+  verDetalle(lista, idx){
+      this.navCtrl.push( DetalleComponent, { lista, idx } )
   }
 }
